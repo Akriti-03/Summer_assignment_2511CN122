d59a3ef55f4a35e517ae9c6e478efb06f84e332f
Aakriti Singh <a7013156@gmail.com>
2026-06-13 11:54:52 +0530

    Day-13 assignment is uploaded

---
+++ Day-13/evenodd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Number of even elements = %d\n", even);
+    printf("Number of odd elements = %d", odd);
+
+    return 0;
+}
