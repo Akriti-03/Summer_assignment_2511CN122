95d9e01769298cb5c050aadbfd26fd859121b964
Aakriti Singh <a7013156@gmail.com>
2026-06-17 18:16:56 +0530

    Day-17 assignment is uploaded

---
+++ Day-17/common.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a[] = {1,2,3,4,5};
+    int b[] = {3,4,5,6,7};
+    int i, j;
+
+    printf("Common Elements:\n");
+
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            if(a[i] == b[j]) {
+                printf("%d ", a[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
