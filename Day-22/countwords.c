8087ae783f9ba5ee8d1927ef9e7c8ff5e9df78c8
Aakriti Singh <a7013156@gmail.com>
2026-06-23 10:56:00 +0530

    Day-22 assignment is uploaded

---
+++ Day-22/countwords.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    char str[100];
+    int i=0, count=1;
+
+    gets(str);
+
+    while(str[i]!='\0') {
+        if(str[i]==' ')
+            count++;
+        i++;
+    }
+
+    printf("Number of words = %d",count);
+
+    return 0;
+}
